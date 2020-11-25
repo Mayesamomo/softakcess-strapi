1b8548c963d5a3744743fdf485ac228e1cdf7921
Mayesamomo <48134632+Mayesamomo@users.noreply.github.com>
2020-11-25 21:57:53 +0000

    refactored

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3553a83b7890777a927453ad5d0e5742'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '823e1650-2cff-47c8-90ef-85a477f0bc98'
+};
